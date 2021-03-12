17bcdd5921acd81546712ef1755e8645ae049fff
Pedro Lacerda <pedrolacerda@github.com>
2021-03-12 17:07:18 -0300

    Finishes exercicio 1

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-let index = require('./routes/index');
-let admin = require('./routes/admin');
-let reservations = require('./routes/reservations');
+const index = require('./routes/index');
+const admin = require('./routes/admin');
+const reservations = require('./routes/reservations');
 
 const auth = require('./lib/middleware/auth.js');
 
-const configuration = {}
-
-
-
-
-
-
-
-
-
-
-
-if (1) {
-
-}
-
-var app = express();
+const app = express();
 
 // Static assets.
 // Move this after the logger if you want to log requests for static assets.
@@ -54,13 +38,13 @@ app.use('/reservations', reservations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
